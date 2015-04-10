4a67068ba06ee28ff51d6ea9533cd85396a5e293
Min RK <benjaminrk@gmail.com>
2015-04-10 16:11:51 -0700

    update js version test

--- jupyter_notebook/tests/base/misc.js
+++ jupyter_notebook/tests/base/misc.js
@@ -5,7 +5,7 @@
 casper.notebook_test(function () {
     var jsver = this.evaluate(function () {
         var cell = IPython.notebook.get_cell(0);
-        cell.set_text('import IPython; print(IPython.__version__)');
+        cell.set_text('import jupyter_notebook; print(jupyter_notebook.__version__)');
         cell.execute();
         return IPython.version;
     });
@@ -15,7 +15,7 @@ casper.notebook_test(function () {
     // refactor this into  just a get_output(0)
     this.then(function () {
         var result = this.get_output_cell(0);
-        this.test.assertEquals(result.text.trim(), jsver, 'IPython.version in JS matches IPython.');
+        this.test.assertEquals(result.text.trim(), jsver, 'IPython.version in JS matches server-side.');
     });
 
 });
