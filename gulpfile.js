0ae3b4377f82c35d89f1461a3fcd09f5e3683bf8
Matthias Bussonnier <bussonniermatthias@gmail.com>
2015-04-15 14:32:16 -0700

    add building sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,11 @@ var less = require('gulp-less');
 var path = require('path');
 var minifyCSS = require('gulp-minify-css');
 var rename = require('gulp-rename');
+var sourcemaps = require('gulp-sourcemaps');
  
-gulp.task('less', function () {
+gulp.task('css', function () {
   return gulp.src('./jupyter_notebook/static/style/*.less')
+    .pipe(sourcemaps.init())
     .pipe(less({
       paths: [ path.join(__dirname, 'less', 'includes') ]
     }))
@@ -13,5 +15,7 @@ gulp.task('less', function () {
     .pipe(rename({
             suffix: '.min'
         }))
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest('./jupyter_notebook/static/style'));
 });
+
