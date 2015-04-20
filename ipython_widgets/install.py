07e9be33ce46cd46cb0640d3db44b6fa5447e141
Jonathan Frederic <jdfreder@calpoly.edu>
2015-04-20 13:11:02 -0700

    :cake: Added a widget JS deprecation warning!
    This is a combination of 9 commits.
    
    Shim IPython.html.widgets
    Add flags to widgets install.
    Put shim in IPython instead
    Add deprecation warning to Javascript source
    Move tests
    Don't use setting to pass log arround.
    Simplify deprecated handler.
    Use requirejs to perform redirect.

--- ipython_widgets/install.py
+++ ipython_widgets/install.py
@@ -3,10 +3,16 @@
 from os.path import dirname, abspath, join as pjoin
 from jupyter_notebook.nbextensions import install_nbextension
 from jupyter_notebook.services.config import ConfigManager
+import argparse
+
+parser = argparse.ArgumentParser(description="Installs the IPython widgets")
+parser.add_argument("-u", "--user", help="Install as current user", action="store_true")
+parser.add_argument("-s", "--symlink", help="Symlink instead of copying files", action="store_true")
+args = parser.parse_args()
 
 print("Installing nbextension ...")
 staticdir = pjoin(dirname(abspath(__file__)), 'static')
-install_nbextension(staticdir, destination='widgets', user=False)
+install_nbextension(staticdir, destination='widgets', user=args.user, symlink=args.symlink)
 
 print("Enabling the extension ...")
 cm = ConfigManager()
