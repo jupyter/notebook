0deee0d9893685598e0b9ef4d9aa880bc923a9c3
Min RK <benjaminrk@gmail.com>
2015-05-04 13:33:39 -0700

    build auth/js/main.min.js
    exports login_main and logout_main

---
+++ notebook/static/auth/js/main.js
@@ -0,0 +1,9 @@
+// Copyright (c) Jupyter Development Team.
+// Distributed under the terms of the Modified BSD License.
+
+define(['./loginmain', './logoutmain'], function (login_main, logout_main) {
+    return {
+        login_main: login_main,
+        logout_main: logout_main
+    };
+});
