2d4b410d34b84202360b1ad09ffd30133c0b1579
Matthias Bussonnier <bussonniermatthias@gmail.com>
2015-05-18 16:53:14 -0700

    rename global to Jupyter

--- notebook/static/base/js/namespace.js
+++ notebook/static/base/js/namespace.js
@@ -1,17 +1,20 @@
 // Copyright (c) Jupyter Development Team.
 // Distributed under the terms of the Modified BSD License.
 
-var IPython = IPython || {};
+var Jupyter = Jupyter || {};
 define(function(){
     var utils = require('base/js/utils');
-    IPython.security = require('base/js/security');
-    IPython.keyboard = require('base/js/keyboard');
-    IPython.dialog = require('base/js/dialog');
-    IPython.utils = utils;
-    IPython.load_extensions = utils.load_extensions;
+    Jupyter.security = require('base/js/security');
+    Jupyter.keyboard = require('base/js/keyboard');
+    Jupyter.dialog = require('base/js/dialog');
+    Jupyter.utils = utils;
+    Jupyter.load_extensions = utils.load_extensions;
 
     "use strict";
-    IPython.version = "4.0.0.dev";
-    IPython._target = '_blank';
-    return IPython;
+    Jupyter.version = "4.0.0.dev";
+    Jupyter._target = '_blank';
+    return Jupyter;
 });
+
+// deprecated since 4.0, remove in 5+
+var IPython = Jupyter
