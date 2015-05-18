36bb3fef27ff1813dea85a686f56374ce07b290f
Matthias Bussonnier <bussonniermatthias@gmail.com>
2015-05-18 16:41:33 -0700

    clean utils of reverse dependency

--- notebook/static/base/js/namespace.js
+++ notebook/static/base/js/namespace.js
@@ -2,7 +2,11 @@
 // Distributed under the terms of the Modified BSD License.
 
 var IPython = IPython || {};
-define([], function(){
+define(function(){
+    var utils = require('base/js/utils');
+    IPython.utils = utils;
+    IPython.load_extensions = utils.load_extensions;
+
     "use strict";
     IPython.version = "4.0.0.dev";
     IPython._target = '_blank';
