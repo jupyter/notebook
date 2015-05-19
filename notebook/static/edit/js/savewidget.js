7c8437659640a613a5ea13ae8c67744152456c63
Matthias Bussonnier <bussonniermatthias@gmail.com>
2015-05-18 17:27:35 -0700

    savewidget does not require namespace

--- notebook/static/edit/js/savewidget.js
+++ notebook/static/edit/js/savewidget.js
@@ -2,13 +2,12 @@
 // Distributed under the terms of the Modified BSD License.
 
 define([
-    'base/js/namespace',
     'jquery',
     'base/js/utils',
     'base/js/dialog',
     'base/js/keyboard',
     'moment',
-], function(IPython, $, utils, dialog, keyboard, moment) {
+], function($, utils, dialog, keyboard, moment) {
     "use strict";
 
     var SaveWidget = function (selector, options) {
