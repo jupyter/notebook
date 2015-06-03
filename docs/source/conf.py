79482e8b4f674736d4971dc6ead1206276c6c0f5
Jonathan Frederic <jon.freder@gmail.com>
2015-06-03 14:46:43 -0700

    Add root

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,7 +20,7 @@ import shlex
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('../..'))
 
 if os.environ.get('READTHEDOCS', ''):
 
