b1f4728d2347d8c1acf0b6529fb5bcaccce9905f
Matthias Bussonnier <bussonniermatthias@gmail.com>
2015-06-07 21:51:12 -0700

    Add some required typescript definition files

---
+++ typings/tsd.d.ts
@@ -0,0 +1,6 @@
+/// <reference path="requirejs/require.d.ts" />
+/// <reference path="ipython/ipython.d.ts" />
+/// <reference path="jquery/jquery.d.ts" />
+/// <reference path="es6-promise/es6-promise.d.ts" />
+/// <reference path="codemirror/codemirror.d.ts" />
+/// <reference path="moment/moment.d.ts" />
