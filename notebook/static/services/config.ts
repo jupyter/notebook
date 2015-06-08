a40d6328d694e4d395593e10c65b0b970cd872ab
Matthias Bussonnier <bussonniermatthias@gmail.com>
2015-06-08 12:40:15 -0700

    add some typing informations

--- notebook/static/services/config.ts
+++ notebook/static/services/config.ts
@@ -9,14 +9,14 @@ import utils = require('base/js/utils');
 
 export class ConfigSection {
 
-    section_name
-    base_url
-    data
-    loaded
-    _one_load_finished
-    _finish_firstload
+    section_name:string
+    base_url:string
+    data:any
+    loaded:Promise<any>
+    _one_load_finished:boolean
+    _finish_firstload:()=>any
 
-    constructor(section_name, options) {
+    constructor(section_name:string, options) {
         this.section_name = section_name;
         this.base_url = options.base_url;
         this.data = {};
@@ -33,18 +33,18 @@ export class ConfigSection {
         });
     }
 
-    public api_url() {
+    public api_url():string {
         return utils.url_join_encode(this.base_url, 'api/config', this.section_name);
     }
     
-    private _load_done() {
+    private _load_done():void {
         if (!this._one_load_finished) {
             this._one_load_finished = true;
             this._finish_firstload();
         }
     }
     
-    public load() {
+    public load():Promise<any> {
         var that = this;
         return utils.promising_ajax(this.api_url(), {
             cache: false,
@@ -105,7 +105,7 @@ export class ConfigWithDefaults {
      * Wait for config to have loaded, then get a value or the default.
      * Returns a promise.
      */
-    public get(key) {
+    public get(key:string) {
         var that = this;
         return this.section.loaded.then(function() {
             return this._class_data()[key] || this.defaults[key]
@@ -116,7 +116,7 @@ export class ConfigWithDefaults {
      * Return a config value. If config is not yet loaded, return the default
      * instead of waiting for it to load.
      */
-    public get_sync(key) {
+    public get_sync(key:string) {
         return this._class_data()[key] || this.defaults[key];
     }
     
@@ -126,7 +126,7 @@ export class ConfigWithDefaults {
      * Returns a promise which is fulfilled when the server replies to the
      * change.
      */
-    public set(key, value) {
+    public set(key:string, value) {
          var d = {};
          d[key] = value;
          if (this.classname) {
