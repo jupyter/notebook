5c26d674d151ab138a41bcd096d322e5960900bd
Matthias Bussonnier <bussonniermatthias@gmail.com>
2015-06-07 22:00:24 -0700

    Add typing files

---
+++ typings/tsd.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="requirejs/require.d.ts" />
+/// <reference path="ipython/ipython.d.ts" />
+/// <reference path="jquery/jquery.d.ts" />
+/// <reference path="es6-promise/es6-promise.d.ts" />
