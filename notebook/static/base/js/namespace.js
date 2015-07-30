1c839f686bf34d59ddaf3b6a2706599f8499e982
Min RK <benjaminrk@gmail.com>
2015-07-30 14:48:35 -0700

    release 4.0.1
    with correct js version this time

--- notebook/static/base/js/namespace.js
+++ notebook/static/base/js/namespace.js
@@ -75,7 +75,7 @@ define(function(){
     jglobal('ClusterList','tree/js/clusterlist');
     jglobal('ClusterItem','tree/js/clusterlist');
 
-    Jupyter.version = "4.0.0.dev";
+    Jupyter.version = "4.0.1";
     Jupyter._target = '_blank';
     return Jupyter;
 });
