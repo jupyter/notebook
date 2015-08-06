073f1c1b0d83e337ee20aec31fa75323cd653eee
Simon Vurens <svurens@gmail.com>
2015-08-06 16:56:19 -0700

    fixed refreshing on bad command

--- notebook/static/notebook/js/commandpalette.js
+++ notebook/static/notebook/js/commandpalette.js
@@ -12,7 +12,7 @@ define(function(require){
         var field = $('<div/>').addClass('typeahead-field');
         var span = $('<span>').addClass('typeahead-query');
         var input = $('<input/>').attr('type', 'search');
-        span.append(input)
+        span.append(input);
         field
             .append(span)
             .append(
@@ -23,31 +23,36 @@ define(function(require){
                 )
             );
 
-        container.append(field)
-        form.append(container)
+        container.append(field);
+        form.append(container);
         var mod = dialog.modal({
             title: 'Execute Action',
             body: $('<div/>').append(form),
             keyboard_manager: notebook.keyboard_manager,
             show: false
         }).on('shown.bs.modal', function () {
-              input.focus()
+              input.focus();
         });
-        
+        var actions = Object.keys(IPython.notebook.keyboard_manager.actions._actions);
         input.typeahead({
             order: "asc",
             source: {
                 groupName: {
-                    data:  Object.keys(IPython.notebook.keyboard_manager.actions._actions)
+                    data:  actions
                 }
             },
             callback: {
                 onInit: function () {console.log('this is init') },
                 onSubmit: function (node, query, result, resultCount) {
                     console.log(node, query, result, resultCount);
-                    console.info(input.val(), 'has been selected')
-                    IPython.notebook.keyboard_manager.actions.call(input.val())
-                    mod.modal('hide')
+                    console.info(input.val(), 'has been selected');
+                    if (actions.indexOf(input.val()) >= 0) {
+                        IPython.notebook.keyboard_manager.actions.call(input.val());
+                    }
+                    else {
+                        console.log("No command " + input.val());
+                    }
+                    mod.modal('hide');
                 }
             }
         })
