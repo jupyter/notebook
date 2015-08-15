930415df3bdf93360980738f3e66382b23100cc8
Min RK <benjaminrk@gmail.com>
2015-08-15 13:41:55 -0700

    release 4.0.3

--- notebook/_version.py
+++ notebook/_version.py
@@ -1,2 +1,2 @@
-version_info = (4, 0, 2)
+version_info = (4, 0, 3)
 __version__ = '.'.join(map(str, version_info))
--- notebook/static/base/js/namespace.js
+++ notebook/static/base/js/namespace.js
@@ -75,7 +75,7 @@ define(function(){
     jglobal('ClusterList','tree/js/clusterlist');
     jglobal('ClusterItem','tree/js/clusterlist');
 
-    Jupyter.version = "4.1.0.dev";
+    Jupyter.version = "4.0.3";
     Jupyter._target = '_blank';
     return Jupyter;
 });
