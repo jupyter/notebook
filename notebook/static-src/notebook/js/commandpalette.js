c0a30d4df8044b4dddb240d37bb8ebdebb5ea240
Jonathan Frederic <jon.freder@gmail.com>
2015-08-17 14:16:49 -0700

    Commandpalette

--- notebook/static-src/notebook/js/commandpalette.js
+++ notebook/static-src/notebook/js/commandpalette.js
@@ -1,7 +1,6 @@
 // Copyright (c) Jupyter Development Team.
 // Distributed under the terms of the Modified BSD License.
 
-define(function(require){
     "use strict";
 
     var QH = require("notebook/js/quickhelp");
@@ -174,5 +173,4 @@ define(function(require){
 
         mod.modal('show');
     };
-    return {'CommandPalette': CommandPalette};
-});
+    module.exports = {'CommandPalette': CommandPalette}; 
