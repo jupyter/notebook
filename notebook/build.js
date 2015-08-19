f2993811b15fb2c679d10a87948bd1d4e3c83695
Jonathan Frederic <jon.freder@gmail.com>
2015-08-18 18:25:20 -0700

    Make sure destination exists

--- notebook/build.js
+++ notebook/build.js
@@ -1,6 +1,8 @@
 var fs = require('fs');
 var aliasify = require('aliasify');
 var browserify = require('browserify');
+var mkdirp = require("mkdirp");
+var path = require('path');
 
 var aliasifyConfig = {
     aliases: {
@@ -19,5 +21,13 @@ var b = browserify({
 });
 
 b.transform(aliasify, aliasifyConfig);
+var mkdirp = require("mkdirp");
 b.add(__dirname + '/static-src/' + process.argv[2]);
-b.bundle().pipe(fs.createWriteStream(__dirname + '/static/' + process.argv[3]));
+var toFile = __dirname + '/static/' + process.argv[3];
+mkdirp(path.dirname(toFile), function(err) { 
+    if (err) {
+        console.error('Could not mkdirp ', err);
+    } else {
+        b.bundle().pipe(fs.createWriteStream(toFile));
+    }
+});
