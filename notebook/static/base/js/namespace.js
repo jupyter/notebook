0600d6a5e65f7de1d77eed6d1c4f3b78df557158
Min RK <benjaminrk@gmail.com>
2015-08-20 21:11:29 -0700

    4.0.4

--- notebook/static/base/js/namespace.js
+++ notebook/static/base/js/namespace.js
@@ -75,7 +75,7 @@ define(function(){
     jglobal('ClusterList','tree/js/clusterlist');
     jglobal('ClusterItem','tree/js/clusterlist');
 
-    Jupyter.version = "4.0.3";
+    Jupyter.version = "4.0.4";
     Jupyter._target = '_blank';
     return Jupyter;
 });
