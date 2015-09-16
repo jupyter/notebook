e5ed972467fbfbf9d40aec108cdc227162635179
Matthias Bussonnier <bussonniermatthias@gmail.com>
2015-09-16 13:46:18 -0700

    Strip more ANSI escape sequence.
    `?` can be found in ANSI escape sequence. ( `[?25l` hide cursor,
    `[25k`show cursor)
    
    Closes #95

--- notebook/tests/base/utils.js
+++ notebook/tests/base/utils.js
@@ -5,6 +5,7 @@ casper.notebook_test(function () {
         "\033[0m[\033[33mwarn\033[0m] \033[0m  https://some/url/to/a/file.ext\033[0m",
         "\033[0m[\033[31merror\033[0m] \033[0m\033[0m",
         "\033[0m[\033[31merror\033[0m] \033[0m\teven more text\033[0m",
+        "[?25hBuilding wheels for collected packages: scipy",
         "\033[0m[\033[31merror\033[0m] \033[0m\t\tand more more text\033[0m"].join("\n");
 
     var output = [
@@ -13,6 +14,7 @@ casper.notebook_test(function () {
         "[<span  class=\"ansiyellow\">warn</span>]   https://some/url/to/a/file.ext",
         "[<span  class=\"ansired\">error</span>] ",
         "[<span  class=\"ansired\">error</span>] \teven more text",
+        "Building wheels for collected packages: scipy",
         "[<span  class=\"ansired\">error</span>] \t\tand more more text"].join("\n");
 
     var result = this.evaluate(function (input) {
