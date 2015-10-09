bb94a6f4a344a8fffd8ec2009039527855894587
Jonathan Frederic <jon.freder@gmail.com>
2015-10-09 14:47:36 -0700

    Reselect multiple cells on menu click, not only a single cell

--- notebook/static/notebook/js/menubar.js
+++ notebook/static/notebook/js/menubar.js
@@ -61,8 +61,9 @@ define([
         this.element.find("li").click(function (event, ui) {
                 // The selected cell loses focus when the menu is entered, so we
                 // re-select it upon selection.
-                var i = that.notebook.get_selected_index();
-                that.notebook.select(i);
+                that.notebook.get_selected_cells().forEach(function(cell) {
+                    cell.select();
+                });
             }
         );
     };
