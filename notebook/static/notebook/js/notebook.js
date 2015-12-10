989c7da9d4e6e05a1d450c69bb01f3453a1be1a0
Jonathan Frederic <jon.freder@gmail.com>
2015-12-10 09:08:00 -0800

    State when the function will be removed

--- notebook/static/notebook/js/notebook.js
+++ notebook/static/notebook/js/notebook.js
@@ -846,8 +846,8 @@ define(function (require) {
      * @return {Notebook} This notebook
      */
     Notebook.prototype.move_cell_up = function (index) {
-        console.warn('Notebook.move_cell_up is deprecated');
-        this.events.trigger('deprecated.Notebook', 'Move cell up is deprecated');
+        console.warn('Notebook.move_cell_up is deprecated as of v4.1 and will be removed in v5.0');
+        this.events.trigger('deprecated.Notebook', 'Move cell up will be removed in v5.0');
         
         var i = this.index_or_selected(index);
         if (this.is_valid_cell_index(i) && i > 0) {
@@ -873,8 +873,8 @@ define(function (require) {
      * @return {Notebook} This notebook
      */
     Notebook.prototype.move_cell_down = function (index) {
-        console.warn('Notebook.move_cell_down is deprecated');
-        this.events.trigger('deprecated.Notebook', 'Move cell down is deprecated');
+        console.warn('Notebook.move_cell_down is deprecated as of v4.1 and will be removed in v5.0');
+        this.events.trigger('deprecated.Notebook', 'Move cell down will be removed in v5.0');
         
         var i = this.index_or_selected(index);
         if (this.is_valid_cell_index(i) && this.is_valid_cell_index(i+1)) {
