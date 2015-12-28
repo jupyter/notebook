0ac253bc1ccd7240d02a5f1d85060c37a18d653a
Fernando Perez <Fernando.Perez@berkeley.edu>
2015-12-27 19:19:58 -0800

    Update keyboard shortcuts page to accurately describe command mode (blue margin)

--- notebook/static/notebook/js/quickhelp.js
+++ notebook/static/notebook/js/quickhelp.js
@@ -189,7 +189,7 @@ define([
             'The Jupyter Notebook has two different keyboard input modes. <b>Edit mode</b> '+
             'allows you to type code/text into a cell and is indicated by a green cell '+
             'border. <b>Command mode</b> binds the keyboard to notebook level actions '+
-            'and is indicated by a grey cell border.'
+            'and is indicated by a grey cell border with a blue left margin.'
         );
         element.append(doc);
         if (platform === 'MacOS') {
