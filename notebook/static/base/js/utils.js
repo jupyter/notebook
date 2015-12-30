ef0f3b9c0e1d1a59d77e20e64d2b957420cc6d9a
Matthias Bussonnier <bussonniermatthias@gmail.com>
2015-12-30 11:28:17 +0100

    Do not allow `<` in url.
    fix #904

--- notebook/static/base/js/utils.js
+++ notebook/static/base/js/utils.js
@@ -370,7 +370,7 @@ define([
 
     // Locate any URLs and convert them to a anchor tag
     function autoLinkUrls(txt) {
-        return txt.replace(/(^|\s)(https?|ftp)(:[^'">\s]+)/gi,
+        return txt.replace(/(^|\s)(https?|ftp)(:[^'"<>\s]+)/gi,
             "$1<a target=\"_blank\" href=\"$2$3\">$2$3</a>");
     }
 
