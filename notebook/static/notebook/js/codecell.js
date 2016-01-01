jupyter/notebook

commit 89b7d96ccd87d03226b7977b469e69172c39ef0e

Fixing way in which selection ranges are deleted

notebook/static/notebook/js/codecell.js
@@ -53,8 +53,9 @@ define([
          if (sel) { 
             var ranges = cm.listSelections();
             for (var i = ranges.length - 1; i >= 0; i--) {
-              var cur = ranges[i].head;
-              cm.replaceRange("", Pos(cur.line, cur.ch - 1), Pos(cur.line, cur.ch + 1));
+              var head = ranges[i].head;
+              var anchor = ranges[i].anchor;
+              cm.replaceRange("", Pos(head.line, head.ch), CodeMirror.Pos(anchor.line, anchor.ch));
             }
             return;
         }

commit 53b8e376337a6271ff4a60514b827781defd06cc

fix for #917, a bug introduced by a typo in 89b7d96ccd87d03226b7977b469e69172c39ef0e

notebook/static/notebook/js/codecell.js
@@ -50,12 +50,12 @@ define([
      */
     CodeMirror.commands.delSpaceToPrevTabStop = function(cm){
         var from = cm.getCursor(true), to = cm.getCursor(false), sel = !posEq(from, to);
-         if (sel) { 
+         if (sel) {
             var ranges = cm.listSelections();
             for (var i = ranges.length - 1; i >= 0; i--) {
-              var head = ranges[i].head;
-              var anchor = ranges[i].anchor;
-              cm.replaceRange("", Pos(head.line, head.ch), CodeMirror.Pos(anchor.line, anchor.ch));
+                var head = ranges[i].head;
+                var anchor = ranges[i].anchor;
+                cm.replaceRange("", CodeMirror.Pos(head.line, head.ch), CodeMirror.Pos(anchor.line, anchor.ch));
             }
             return;
         }