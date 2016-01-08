3754c28721eacd01ede120468c3dd27eb6710eb7
Matthias Bussonnier <bussonniermatthias@gmail.com>
2016-01-08 19:09:51 +0100

    back on dev

--- notebook/static/base/js/namespace.js
+++ notebook/static/base/js/namespace.js
@@ -73,7 +73,7 @@ define(function(){
     // tree
     jglobal('SessionList','tree/js/sessionlist');
 
-    Jupyter.version = "4.1.0";
+    Jupyter.version = "4.2.0dev";
     Jupyter._target = '_blank';
     return Jupyter;
 });
