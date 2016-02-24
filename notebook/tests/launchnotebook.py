ba161b06dd42ab12e6e13d1bae8e63d3fe0a847a
Min RK <benjaminrk@gmail.com>
2016-02-24 13:45:00 +0100

    teardown zmq Context explicitly at the end of each test group
    Increases the chances of noticing when we aren't cleaning up our sockets properly.

--- notebook/tests/launchnotebook.py
+++ notebook/tests/launchnotebook.py
@@ -18,6 +18,7 @@ except ImportError:
     from mock import patch #py2
 
 from tornado.ioloop import IOLoop
+import zmq
 
 import jupyter_core.paths
 from ..notebookapp import NotebookApp
@@ -131,6 +132,16 @@ class NotebookTestBase(TestCase):
         cls.notebook_dir.cleanup()
         cls.env_patch.stop()
         cls.path_patch.stop()
+        # cleanup global zmq Context, to ensure we aren't leaving dangling sockets
+        def cleanup_zmq():
+            zmq.Context.instance().term()
+        t = Thread(target=cleanup_zmq)
+        t.daemon = True
+        t.start()
+        t.join(5) # give it a few seconds to clean up (this should be immediate)
+        # if term never returned, there's zmq stuff still open somewhere, so shout about it.
+        if t.is_alive():
+            raise RuntimeError("Failed to teardown zmq Context, open sockets likely left lying around.")
 
     @classmethod
     def base_url(cls):
