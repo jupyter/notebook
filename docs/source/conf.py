3cdc082ab27842276f35ca1e3ade05c025e1b117
Matthias Geier <Matthias.Geier@gmail.com>
2016-03-26 14:28:58 +0100

    Ignore .ipynb_checkpoints/ when building docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -118,7 +118,7 @@ language = None
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build']
+exclude_patterns = ['examples/Notebook/.ipynb_checkpoints']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
