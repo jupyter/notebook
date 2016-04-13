f10ce5d4ed99672d363e63ab3eab7c6663681458
Min RK <benjaminrk@gmail.com>
2016-04-13 15:46:44 +0200

    Backport PR #1237: Allow a session to connect to an existing kernel

--- notebook/services/sessions/sessionmanager.py
+++ notebook/services/sessions/sessionmanager.py
@@ -69,10 +69,14 @@ class SessionManager(LoggingConfigurable):
         return unicode_type(uuid.uuid4())
 
     @gen.coroutine
-    def create_session(self, path=None, kernel_name=None):
+    def create_session(self, path=None, kernel_name=None, kernel_id=None):
         """Creates a session and returns its model"""
         session_id = self.new_session_id()
-        kernel_id = yield self.start_kernel_for_session(session_id, path, kernel_name)
+        if kernel_id is not None and kernel_id in self.kernel_manager:
+            pass
+        else:
+            kernel_id = yield self.start_kernel_for_session(session_id, path,
+                                                            kernel_name)
         result = yield gen.maybe_future(
             self.save_session(session_id, path=path, kernel_id=kernel_id)
         )
