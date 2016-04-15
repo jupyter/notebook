02f287bab2e6c2cb96fd5bd218abbd45ca4e029d
Min RK <benjaminrk@gmail.com>
2016-04-15 21:17:10 +0200

    release 4.2.0

--- notebook/static/base/js/namespace.js
+++ notebook/static/base/js/namespace.js
@@ -73,7 +73,7 @@ define(function(){
     // tree
     jglobal('SessionList','tree/js/sessionlist');
 
-    Jupyter.version = "4.2.0b1";
+    Jupyter.version = "4.2.0";
     Jupyter._target = '_blank';
     return Jupyter;
 });
