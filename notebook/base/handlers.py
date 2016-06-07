jupyter/notebook

commit 65eb248209b5fdc9d1d430c0403fc6bfee1057f4

Add the method to the correct base class

notebook/base/handlers.py
@@ -386,6 +386,13 @@ class APIHandler(IPythonHandler):
         self.set_header('Content-Type', 'application/json')
         return super(APIHandler, self).finish(*args, **kwargs)
 
+    @web.authenticated
+    def options(self, *args, **kwargs):
+        self.set_header('Access-Control-Allow-Headers', 'accept, content-type')
+        self.set_header('Access-Control-Allow-Methods',
+                        'GET, PUT, PATCH, DELETE, OPTIONS')
+        self.finish()
+
 
 class Template404(IPythonHandler):
     """Render our 404 template"""

notebook/services/api/handlers.py
@@ -18,13 +18,6 @@ class APIHandler(web.StaticFileHandler, IPythonHandler):
         self.set_header('Content-Type', 'text/x-yaml')
         return web.StaticFileHandler.get(self, 'api.yaml')
 
-    @web.authenticated
-    def options(self, *args, **kwargs):
-        self.set_header('Access-Control-Allow-Headers', 'accept, content-type')
-        self.set_header('Access-Control-Allow-Methods',
-                        'GET, PUT, POST, PATCH, DELETE, OPTIONS')
-        self.finish()
-
 default_handlers = [
     (r"/api/spec.yaml", APIHandler)
 ]

commit 144807bc3c8a1c17722f9c072936b67897f7b0dd

Allow requests to POST in OPTIONS requests
Commit 65eb248209b5fdc9d1d430c0403fc6bfee1057f4 introduced a bug by not allowing the POST method in OPTIONS requests (which is wrong since several parts of the API accept POST requests, for example creating a new kernel).

notebook/base/handlers.py
@@ -390,7 +390,7 @@ class APIHandler(IPythonHandler):
     def options(self, *args, **kwargs):
         self.set_header('Access-Control-Allow-Headers', 'accept, content-type')
         self.set_header('Access-Control-Allow-Methods',
-                        'GET, PUT, PATCH, DELETE, OPTIONS')
+                        'GET, PUT, POST, PATCH, DELETE, OPTIONS')
         self.finish()
 
 