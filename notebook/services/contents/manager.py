6768b8940749f8d1a586e77b8767f6339a081c58
Min RK <benjaminrk@gmail.com>
2016-07-06 14:01:34 +0200

    fix notebook validation
    validate was imported from traitlets in #1527, clobbering validate from nbformat

--- notebook/services/contents/manager.py
+++ notebook/services/contents/manager.py
@@ -13,7 +13,7 @@ from tornado.web import HTTPError
 
 from .checkpoints import Checkpoints
 from traitlets.config.configurable import LoggingConfigurable
-from nbformat import sign, validate, ValidationError
+from nbformat import sign, validate as validate_nb, ValidationError
 from nbformat.v4 import new_notebook
 from ipython_genutils.importstring import import_item
 from traitlets import (
@@ -299,7 +299,7 @@ class ContentsManager(LoggingConfigurable):
     def validate_notebook_model(self, model):
         """Add failed-validation message to model"""
         try:
-            validate(model['content'])
+            validate_nb(model['content'])
         except ValidationError as e:
             model['message'] = u'Notebook Validation failed: {}:\n{}'.format(
                 e.message, json.dumps(e.instance, indent=1, default=lambda obj: '<UNKNOWN>'),
