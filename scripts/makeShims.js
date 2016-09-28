da264da982d3e650a06b6d68ef8ede6a8360ad18
Steven Silvester <steven.silvester@ieee.org>
2016-09-28 14:51:11 -0500

    Add docs and avoid hard-coded name

--- scripts/makeShims.js
+++ scripts/makeShims.js
@@ -2,19 +2,25 @@ var path = require('path');
 var fs = require('fs');
 
 
+/**
+ * Make a shim file for a built notebook bundle that re-exports
+ * each module to an AMD module.
+ *
+ * @param name (string) - The name of the output bundle.
+ */
 function makeShim(name) {
     var manifest = require('../notebook/static/' + name + '/js/built/vendor-manifest.json');
     var content = [];
     for (var key in manifest.content) {
-        var num = manifest.content[key];
+        var num = String(manifest.content[key]);
         if (path.extname(key) !== '.js') {
             continue;
         }
         key = key.replace('./notebook/static/', '');
         key = key.replace('./node_modules/', '');
         key = key.replace('.js', '');
-        var line = 'define("' + key + '", function () { return vendor(';
-        line += String(num) + '); });'
+        var line = 'define("' + key + '", function () { return ';
+        line += manifest.name + '(' + num + '); });'
         content.push(line);
     }
     var outputPath = './notebook/static/' + name + '/js/built/shim.bundle.js';
