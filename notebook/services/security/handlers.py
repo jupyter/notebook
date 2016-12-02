ab791441b23458909e92cd7a6881eb2fca393def
Kyle Kelley <rgbkrk@gmail.com>
2016-12-02 09:50:36 -0800

    Backport PR #1938: don't check origin on token-authenticated requests
    adds LoginHandler.should_check_origin classmethod API
    
    While testing, I noticed that we were checking origin and authentication on CSP violation reports, but browsers are sending CSP reports with no Origin, meaning that no CSP report will ever be accepted. That doesn't make sense to me, so I disabled both - a CSP report can now be made from *anywhere* (no auth, no check).  rgbkrk does that sound right to you? Or should it be authenticated but no origin check?
    
    Signed-off-by: Min RK <benjaminrk@gmail.com>

--- notebook/services/security/handlers.py
+++ notebook/services/security/handlers.py
@@ -3,19 +3,23 @@
 # Copyright (c) Jupyter Development Team.
 # Distributed under the terms of the Modified BSD License.
 
-from tornado import gen, web
+from tornado import web
 
 from ...base.handlers import APIHandler, json_errors
 from . import csp_report_uri
 
 class CSPReportHandler(APIHandler):
     '''Accepts a content security policy violation report'''
+
+    def skip_origin_check(self):
+        """Don't check origin when reporting origin-check violations!"""
+        return True
+
     @json_errors
     @web.authenticated
     def post(self):
         '''Log a content security policy violation report'''
-        csp_report = self.get_json_body()
-        self.log.warn("Content security violation: %s",
+        self.log.warning("Content security violation: %s",
                       self.request.body.decode('utf8', 'replace'))
 
 default_handlers = [
