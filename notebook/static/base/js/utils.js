a2f7325537fa7b92040e88856f29f309f823a22e
Min RK <benjaminrk@gmail.com>
2016-12-16 13:50:42 +0100

    fix carriage return handling
    The only real fix is an errant `+` on the final replacement, which would end up skipping sequential replacements.
    
    - leaves trailing `\r` on the text, if there is one
    - use groups to avoid unnecessary replace calls (no change)
    - includes test

--- notebook/static/base/js/utils.js
+++ notebook/static/base/js/utils.js
@@ -445,11 +445,11 @@ define([
     // carriage return characters
     function fixCarriageReturn(txt) {
         txt = txt.replace(/\r+\n/gm, '\n'); // \r followed by \n --> newline
-        while (txt.search(/\r/g) > -1) {
-            var base = txt.match(/^.*\r+/m)[0].replace(/\r/, '');
-            var insert = txt.match(/\r+.*$/m)[0].replace(/\r/, '');
+        while (txt.search(/\r[^$]/g) > -1) {
+            var base = txt.match(/^(.*)\r+/m)[1];
+            var insert = txt.match(/\r+(.*)$/m)[1];
             insert = insert + base.slice(insert.length, base.length);
-            txt = txt.replace(/\r+.*$/m, '\r').replace(/^.*\r+/m, insert);
+            txt = txt.replace(/\r+.*$/m, '\r').replace(/^.*\r/m, insert);
         }
         return txt;
     }
