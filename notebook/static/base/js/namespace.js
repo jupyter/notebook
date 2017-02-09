3b65aae93b580636a1289c6aba8c64687e1b852b
Grant Nestor <grantnestor@gmail.com>
2017-02-08 20:04:55 -0800

    release 4.4.0

--- notebook/static/base/js/namespace.js
+++ notebook/static/base/js/namespace.js
@@ -73,7 +73,7 @@ define(function(){
     // tree
     jglobal('SessionList','tree/js/sessionlist');
 
-    Jupyter.version = "4.3.2";
+    Jupyter.version = "4.4.0";
     Jupyter._target = '_blank';
     return Jupyter;
 });
