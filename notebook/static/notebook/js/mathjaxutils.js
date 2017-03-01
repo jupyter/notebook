3b50d4b4ad8bd0df68876bd60186b86a41ccc857
Lorenzo Gasparini <gasparini.lorenzo@gmail.com>
2017-03-01 18:53:34 +0100

    Add comments

--- notebook/static/notebook/js/mathjaxutils.js
+++ notebook/static/notebook/js/mathjaxutils.js
@@ -204,6 +204,11 @@ define([
     //    and clear the math array (no need to keep it around).
     //
     var replace_math = function (text, math) {
+    	//
+    	//  Replaces a math placeholder with its corresponding group.
+    	//  The math delimiters "\\(", "\\[", "\\)" and "\\]" are replaced
+    	//  removing one backslash in order to be interpreted correctly by MathJax.
+    	//
         var math_group_process = function (match, n) {
             var math_group = math[n];
 
@@ -216,6 +221,8 @@ define([
             return math_group;
         };
 
+        // Replace all the math group placeholders in the text
+        // with the saved strings.
         text = text.replace(/@@(\d+)@@/g, math_group_process);
         
         return text;
