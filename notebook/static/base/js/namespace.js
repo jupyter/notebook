048f243a3a8d8de36acfca26070cfe22ec7362bb
Grant Nestor <grantnestor@gmail.com>
2017-03-31 11:22:53 -0700

    release 5.0.0

--- notebook/static/base/js/namespace.js
+++ notebook/static/base/js/namespace.js
@@ -73,7 +73,7 @@ define(function(){
     // tree
     jglobal('SessionList','tree/js/sessionlist');
 
-    Jupyter.version = "5.0.0.dev";
+    Jupyter.version = "5.0.0";
     Jupyter._target = '_blank';
     return Jupyter;
 });
