124e38d992dc6b304a52d280ab691f00976af7d1
Kirit Thadaka <kirit.thadaka@gmail.com>
2017-12-01 10:13:07 -0500

    Changed delete_file function to allow deletion of non empty directories

--- notebook/services/contents/filemanager.py
+++ notebook/services/contents/filemanager.py
@@ -489,16 +489,8 @@ class FileContentsManager(FileManagerMixin, ContentsManager):
         path = path.strip('/')
         os_path = self._get_os_path(path)
         rm = os.unlink
-        if os.path.isdir(os_path):
-            listing = os.listdir(os_path)
-            # Don't delete non-empty directories.
-            # A directory containing only leftover checkpoints is
-            # considered empty.
-            cp_dir = getattr(self.checkpoints, 'checkpoint_dir', None)
-            for entry in listing:
-                if entry != cp_dir:
-                    raise web.HTTPError(400, u'Directory %s not empty' % os_path)
-        elif not os.path.isfile(os_path):
+        
+        if not os.path.isfile(os_path):
             raise web.HTTPError(404, u'File does not exist: %s' % os_path)
 
         if self.delete_to_trash:
@@ -517,6 +509,40 @@ class FileContentsManager(FileManagerMixin, ContentsManager):
             self.log.debug("Unlinking file %s", os_path)
             with self.perm_to_403():
                 rm(os_path)
+
+    # def delete_file(self, path):
+    #     """Delete file at path."""
+    #     path = path.strip('/')
+    #     os_path = self._get_os_path(path)
+    #     rm = os.unlink
+    #     if os.path.isdir(os_path):
+    #         listing = os.listdir(os_path)
+    #         # Don't delete non-empty directories.
+    #         # A directory containing only leftover checkpoints is
+    #         # considered empty.
+    #         cp_dir = getattr(self.checkpoints, 'checkpoint_dir', None)
+    #         for entry in listing:
+    #             if entry != cp_dir:
+    #                 raise web.HTTPError(400, u'Directory %s not empty' % os_path)
+    #     elif not os.path.isfile(os_path):
+    #         raise web.HTTPError(404, u'File does not exist: %s' % os_path)
+
+    #     if self.delete_to_trash:
+    #         self.log.debug("Sending %s to trash", os_path)
+    #         # Looking at the code in send2trash, I don't think the errors it
+    #         # raises let us distinguish permission errors from other errors in
+    #         # code. So for now, just let them all get logged as server errors.
+    #         send2trash(os_path)
+    #         return
+
+    #     if os.path.isdir(os_path):
+    #         self.log.debug("Removing directory %s", os_path)
+    #         with self.perm_to_403():
+    #             shutil.rmtree(os_path)
+    #     else:
+    #         self.log.debug("Unlinking file %s", os_path)
+    #         with self.perm_to_403():
+    #             rm(os_path)
 
     def rename_file(self, old_path, new_path):
         """Rename a file."""
