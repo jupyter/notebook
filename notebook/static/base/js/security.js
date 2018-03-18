17df0f88489be6cd1ed65dfd9bbed3e9ece3fea4
Thomas Kluyver <takowl@gmail.com>
2018-03-18 08:00:12 +0000

    Disable jQuery prefilter specifically to parse sanitized HTML (#29)
    * Add and use sanitize_html_and_parse() function
    
    * Describe return type in doc comment
    
    * Reset htmlPrefilter even if parsing HTML fails
    
    * Return directly from try block
    
    * Move toinsert to inner scope

--- notebook/static/base/js/security.js
+++ notebook/static/base/js/security.js
@@ -122,9 +122,29 @@ define([
         
         return sanitized;
     };
+
+    var sanitize_html_and_parse = function (html, allow_css) {
+        /**
+         * Sanitize HTML and parse it safely using jQuery.
+         *
+         * This disable's jQuery's html 'prefilter', which can make invalid
+         * HTML valid after the sanitizer has checked it.
+         *
+         * Returns an array of DOM nodes.
+         */
+        var sanitized_html = sanitize_html(html, allow_css);
+        var prev_htmlPrefilter = $.htmlPrefilter;
+        $.htmlPrefilter = function(html) {return html;};  // Don't modify HTML
+        try {
+            return $.parseHTML(sanitized_html);
+        } finally {
+            $.htmlPrefilter = prev_htmlPrefilter;  // Set it back again
+        }
+    };
     
     var security = {
         caja: caja,
+        sanitize_html_and_parse: sanitize_html_and_parse,
         sanitize_html: sanitize_html
     };
 
