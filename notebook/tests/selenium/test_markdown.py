c16dac22ca2ab9e017e33a0b968b3a9cb32c04b0
M Pacer <mpacer@berkeley.edu>
2018-03-23 08:33:47 -0700

    use new rich container __*__ methods to make adding & executing nicer

--- notebook/tests/selenium/test_markdown.py
+++ notebook/tests/selenium/test_markdown.py
@@ -15,11 +15,10 @@ def notebook(authenticated_browser):
 
 def test_markdown_cell(notebook):
     nb = notebook
-    cell = nb.cells[0]
-    nb.convert_cell_type(cell_type="markdown")
-    assert nb.current_cell != cell
-    nb.edit_cell(index=0, content="# Foo")
-    nb.wait_for_stale_cell(cell)
+    nb[:] = ["# Foo"]
+    nb.convert_cell_type(0, cell_type="markdown")
+    for cell in nb:
+        nb.execute_cell(cell)
     rendered_cells = nb.browser.find_elements_by_class_name('text_cell_render')
     outputs = [x.get_attribute('innerHTML') for x in rendered_cells]
     expected = '<h1 id="Foo">Foo<a class="anchor-link" href="#Foo">¶</a></h1>'
