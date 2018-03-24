5677e22f5945a38d36a7801296d1c2550221a21e
hkshesht <hisham.elsheshtawy@gmail.com>
2018-03-24 16:04:34 -0400

    Initial commit to convert deletecell.js to selenium

---
+++ notebook/tests/selenium/test_deletecell.py
@@ -0,0 +1,16 @@
+import os
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+pjoin = os.path.join
+
+def cell_is_deletable(index):
+    pass
+
+def test_deletable_cells(browser):
+    pass
+
+def test_non_deletable_cells(browser):
+    pass
