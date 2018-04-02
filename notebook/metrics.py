7bad76217716b91c8f46de46cad62c141d62fea0
yuvipanda <yuvipanda@gmail.com>
2018-04-02 12:21:44 -0700

    Use regular dashes, not em (or en?) dashes
    Works in JupyterHub because python3, fails python2 test here.

--- notebook/metrics.py
+++ notebook/metrics.py
@@ -21,9 +21,9 @@ def prometheus_log_method(handler):
     Tornado log handler for recording RED metrics.
 
     We record the following metrics:
-       Rate – the number of requests, per second, your services are serving.
-       Errors – the number of failed requests per second.
-       Duration – The amount of time each request takes expressed as a time interval.
+       Rate - the number of requests, per second, your services are serving.
+       Errors - the number of failed requests per second.
+       Duration - The amount of time each request takes expressed as a time interval.
 
     We use a fully qualified name of the handler as a label,
     rather than every url path to reduce cardinality.
