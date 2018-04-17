5361b723191eeb411cfc9dbf007f72ce5dd08b89
Rosa Swaby <rs4791@nyu.edu>
2018-04-17 11:37:48 -0400

    removed focus on token input

--- notebook/static/auth/js/loginmain.js
+++ notebook/static/auth/js/loginmain.js
@@ -6,8 +6,6 @@ define(['jquery', 'base/js/namespace', 'base/js/page'], function($, IPython, pag
       var page_instance = new page.Page('div#header', 'div#site');
       page_instance.show();
       $('input#password_input').focus();
-      $('input#token_input').focus();
-
       IPython.page = page_instance;
     }
     return login_main;
