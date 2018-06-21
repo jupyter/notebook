b428d2d40a465c467496142ddc58dff4b7788918
Thomas Kluyver <thomas@kluyver.me.uk>
2018-06-21 09:49:59 +0200

    release 5.6.0rc1

--- notebook/static/base/js/namespace.js
+++ notebook/static/base/js/namespace.js
@@ -73,7 +73,7 @@ define(function(){
     // tree
     jglobal('SessionList','tree/js/sessionlist');
 
-    Jupyter.version = "5.6.0.dev0";
+    Jupyter.version = "5.6.0rc1";
     Jupyter._target = '_blank';
     return Jupyter;
 });
