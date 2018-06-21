b8b66332e2023e83d2ee04f83d8814f567e01a4e
Thomas Kluyver <thomas@kluyver.me.uk>
2018-06-21 09:51:37 +0200

    Back to development

--- notebook/_version.py
+++ notebook/_version.py
@@ -9,5 +9,5 @@ store the current version info of the notebook.
 
 # Next beta/alpha/rc release: The version number for beta is X.Y.ZbN **without dots**. 
 
-version_info = (5, 6, 0, 'rc1')
+version_info = (5, 6, 0, '.dev0')
 __version__ = '.'.join(map(str, version_info[:3])) + ''.join(version_info[3:])
