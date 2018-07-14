2dd46b16c9a1c683198c6e840759c682012f3c84
Thomas Kluyver <thomas@kluyver.me.uk>
2018-07-14 13:43:16 +0200

    back to development

--- notebook/_version.py
+++ notebook/_version.py
@@ -9,5 +9,5 @@ store the current version info of the notebook.
 
 # Next beta/alpha/rc release: The version number for beta is X.Y.ZbN **without dots**. 
 
-version_info = (5, 6, 0)
+version_info = (5, 7, 0, '.dev0')
 __version__ = '.'.join(map(str, version_info[:3])) + ''.join(version_info[3:])
--- notebook/static/base/js/namespace.js
+++ notebook/static/base/js/namespace.js
@@ -73,7 +73,7 @@ define(function(){
     // tree
     jglobal('SessionList','tree/js/sessionlist');
 
-    Jupyter.version = "5.6.0";
+    Jupyter.version = "5.7.0.dev0";
     Jupyter._target = '_blank';
     return Jupyter;
 });
