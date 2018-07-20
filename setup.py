f9d578ee405e767d7d52d6bf028f61ea54011682
Madicken Munk <madicken.munk@gmail.com>
2018-07-20 16:52:21 -0500

    add pytest to test env requirements and suggest it in contributing docs

--- setup.py
+++ setup.py
@@ -97,7 +97,7 @@ for more information.
         ':python_version == "2.7"': ['ipaddress'],
         'test:python_version == "2.7"': ['mock'],
         'test': ['nose', 'coverage', 'requests', 'nose_warnings_filters',
-                 'nbval', 'nose-exclude', 'selenium'],
+                 'nbval', 'nose-exclude', 'selenium', 'pytest', 'pytest-cov'],
         'test:sys_platform == "win32"': ['nose-exclude'],
     },
     entry_points = {
