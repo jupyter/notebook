e55edce2658851c40be103e9652e02afdb47e29d
Remi Rampin <remirampin@gmail.com>
2018-07-24 15:11:06 -0400

    Add test for bcrypt

--- notebook/auth/tests/test_security.py
+++ notebook/auth/tests/test_security.py
@@ -4,10 +4,9 @@ import nose.tools as nt
 
 def test_passwd_structure():
     p = passwd('passphrase')
-    algorithm, salt, hashed = p.split(':')
-    nt.assert_equal(algorithm, 'sha1')
-    nt.assert_equal(len(salt), salt_len)
-    nt.assert_equal(len(hashed), 40)
+    algorithm, hashed = p.split(':')
+    nt.assert_equal(algorithm, 'bcrypt')
+    nt.assert_true(hashed.startswith('$2b$'))
 
 def test_roundtrip():
     p = passwd('passphrase')
@@ -22,4 +21,7 @@ def test_bad():
 def test_passwd_check_unicode():
     # GH issue #4524
     phash = u'sha1:23862bc21dd3:7a415a95ae4580582e314072143d9c382c491e4f'
+    assert passwd_check(phash, u"łe¶ŧ←↓→")
+    phash = (u'bcrypt:$2b$12$'
+             u'/IwxT/HWZRlDczICrZQVr.Mg0K5eu9Sv817lgf8qUd8goygYs1OlO')
     assert passwd_check(phash, u"łe¶ŧ←↓→")
