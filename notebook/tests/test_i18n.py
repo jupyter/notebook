a46a849450dad6ed22e37464ec33594669b6ca4d
Min RK <benjaminrk@gmail.com>
2018-09-26 16:10:02 +0200

    test that front & backend translations are in sync
    both respect the Accept-Language header

--- notebook/tests/test_i18n.py
+++ notebook/tests/test_i18n.py
@@ -1,6 +1,12 @@
+"""Tests for translation utilities"""
+import json
+import re
+
 import pytest
 
 from notebook import i18n
+
+from .launchnotebook import NotebookTestBase
 
 def test_parse_accept_lang_header():
     palh = i18n.parse_accept_lang_header
@@ -32,3 +38,53 @@ def test_parse_accept_lang_header():
 )
 def test_filter_languages(languages, expected):
     assert i18n.filter_languages(languages) == expected
+
+
+class TestAcceptLanguageHeaders(NotebookTestBase):
+
+    def test_notranslate(self):
+        headers = {"Accept-Language": ""}
+        r = self.request("GET", "/tree/", headers=headers)
+        r.raise_for_status()
+        html = r.text
+        nbjs_translations = self.find_nbjs_translations(html)
+        assert list(nbjs_translations.keys()) == ['']
+
+    def find_nbjs_translations(self, html):
+        """Find the nbjs_translations object in the HTML page
+
+        Returns the nbjs locale data
+        """
+        m = re.search(r"nbjs_translations\s*=\s*(.*);", html)
+        if m is None:
+            return None
+        translations = json.loads(m.group(1))
+        return translations['locale_data']['nbjs']
+
+    def test_tree_fr(self):
+        headers = {"Accept-Language": "fr-FR;q=0.9,en;q=0.5"}
+        r = self.request("GET", "/tree/", headers=headers)
+        r.raise_for_status()
+        html = r.text
+        # test a frontend translation
+        nbjs_translations = self.find_nbjs_translations(html)
+        assert '(autosaved)' in nbjs_translations
+        assert nbjs_translations['(autosaved)'] == ['(auto-sauvegardé)']
+        # and a backend one
+        trans = i18n.get_translation("nbui", ["fr_FR"])
+        assert trans.gettext("Files") != "Files"
+        assert trans.gettext("Files") in html
+
+    def test_tree_zh(self):
+        headers = {"Accept-Language": "zh-CN;q=0.9,en;q=0.5"}
+        r = self.request("GET", "/tree/", headers=headers)
+        r.raise_for_status()
+        html = r.text
+        # test a frontend translation
+        nbjs_translations = self.find_nbjs_translations(html)
+        assert '(autosaved)' in nbjs_translations
+        assert nbjs_translations['(autosaved)'] == ['(自动保存)']
+        # and a backend one
+        trans = i18n.get_translation("nbui", ["zh_CN"])
+        assert trans.gettext("Files") != "Files"
+        assert trans.gettext("Files") in html
