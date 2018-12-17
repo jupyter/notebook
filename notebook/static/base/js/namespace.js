fe7c29096f77283b0c7097f810ee54b680956306
Thomas Kluyver <thomas@kluyver.me.uk>
2018-12-17 11:01:51 +0100

    release 5.7.4

--- notebook/static/base/js/namespace.js
+++ notebook/static/base/js/namespace.js
@@ -73,7 +73,7 @@ define(function(){
     // tree
     jglobal('SessionList','tree/js/sessionlist');
 
-    Jupyter.version = "5.7.4.dev0";
+    Jupyter.version = "5.7.4";
     Jupyter._target = '_blank';
     return Jupyter;
 });
