05aa4b2cffb55896b53f0a5e50bc9bca405cb29f
Min RK <benjaminrk@gmail.com>
2019-03-08 13:26:23 +0100

    release 5.7.6

--- notebook/static/base/js/namespace.js
+++ notebook/static/base/js/namespace.js
@@ -73,7 +73,7 @@ define(function(){
     // tree
     jglobal('SessionList','tree/js/sessionlist');
 
-    Jupyter.version = "5.7.5";
+    Jupyter.version = "5.7.6";
     Jupyter._target = '_blank';
     return Jupyter;
 });
