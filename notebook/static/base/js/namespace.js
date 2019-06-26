e771c9d1c841164a23b21891ccf8c7dc5f459ae0
Luciano Resende <lresende@apache.org>
2019-06-26 07:56:45 -0700

    Release 6.0.0rc1

--- notebook/static/base/js/namespace.js
+++ notebook/static/base/js/namespace.js
@@ -73,7 +73,7 @@ define(function(){
     // tree
     jglobal('SessionList','tree/js/sessionlist');
 
-    Jupyter.version = "6.0.0.dev0";
+    Jupyter.version = "6.0.0rc1";
     Jupyter._target = '_blank';
 
     return Jupyter;
