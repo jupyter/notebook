6144fbe4bc8dee8d039030bc4939ede2091f3d39
Babatunde Adeola <babatundeadeola@gmail.com>
2019-07-12 16:05:08 +0100

    Add if statement to focus button

--- notebook/static/base/js/dialog.js
+++ notebook/static/base/js/dialog.js
@@ -130,7 +130,9 @@ define(['jquery',
                 options.keyboard_manager.enable();
                 options.keyboard_manager.command_mode();
             }
-            $(options.focus_button).focus();
+	    if (options.focus_button) {
+            	$(options.focus_button).focus();
+	    }
         });
         
         if (options.keyboard_manager) {
