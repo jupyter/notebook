b4acc47d4c00e02960dbf906de74f9973ff19524
Luciano Resende <lresende@apache.org>
2019-11-04 12:29:01 -0800

    Move license information from COPYING.md
    Closes #4765
    Fixes 2768

--- notebook/log.py
+++ notebook/log.py
@@ -2,7 +2,7 @@
 #  Copyright (c) Jupyter Development Team
 #
 #  Distributed under the terms of the BSD License.  The full license is in
-#  the file COPYING, distributed as part of this software.
+#  the file LICENSE, distributed as part of this software.
 #-----------------------------------------------------------------------------
 
 import json
