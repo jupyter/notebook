a885803988535062f2791acbdc35b1190c333b76
Madhusudhan Srinivasa <me@madhums.me>
2019-12-17 21:59:39 +0100

    use cmdtrl-enter to run a cell
    Instead of ctrl-enter to run a cell, use cmdtrl-enter - this will provide better ux for mac users.

--- notebook/static/notebook/js/keyboardmanager.js
+++ notebook/static/notebook/js/keyboardmanager.js
@@ -104,9 +104,9 @@ define([
         return {
             'shift'       : 'jupyter-notebook:ignore',
             'shift-enter' : 'jupyter-notebook:run-cell-and-select-next',
-            'ctrl-enter'  : 'jupyter-notebook:run-cell',
             'alt-enter'   : 'jupyter-notebook:run-cell-and-insert-below',
             // cmd on mac, ctrl otherwise
+            'cmdtrl-enter'  : 'jupyter-notebook:run-cell',
             'cmdtrl-s'    : 'jupyter-notebook:save-notebook'
         };
     };
