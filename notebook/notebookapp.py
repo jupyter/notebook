7a13cd12f66cad735bd9df58637fa826f4fadc65
Kevin Bates <kbates4@gmail.com>
2020-02-11 08:15:46 -0800

    Add env variable support for port options
    In order to better support use cases relating to containerized environments,
    this change adds environment variable support for the `--port` option.  Since
    anyone setting a specific port probably doesn't want port retry logic enabled,
    the `--port-retries` option has also been backed by an env.
    
    Option `--port` will be backed by env `JUPYTER_PORT` and still defaults to `8888`.
    Option `--port-retries` will be backed by env `JUPYTER_PORT_RETRIES` and still
    defaults to `50`.
    
    The CLI options will override those set via the envrionment, but environment
    values override those set via configuration files.
    
    Closes #5212

--- notebook/notebookapp.py
+++ notebook/notebookapp.py
@@ -707,13 +707,26 @@ class NotebookApp(JupyterApp):
         or containerized setups for example).""")
     )
 
-    port = Integer(8888, config=True,
-        help=_("The port the notebook server will listen on.")
+    port_env = 'JUPYTER_PORT'
+    port_default_value = 8888
+    port = Integer(port_default_value, config=True,
+        help=_("The port the notebook server will listen on (env: JUPYTER_PORT).")
     )
 
-    port_retries = Integer(50, config=True,
-        help=_("The number of additional ports to try if the specified port is not available.")
+    @default('port')
+    def port_default(self):
+        return int(os.getenv(self.port_env, self.port_default_value))
+
+    port_retries_env = 'JUPYTER_PORT_RETRIES'
+    port_retries_default_value = 50
+    port_retries = Integer(port_retries_default_value, config=True,
+        help=_("The number of additional ports to try if the specified port is not "
+               "available (env: JUPYTER_PORT_RETRIES).")
     )
+
+    @default('port_retries')
+    def port_retries_default(self):
+        return int(os.getenv(self.port_retries_env, self.port_retries_default_value))
 
     certfile = Unicode(u'', config=True, 
         help=_("""The full path to an SSL/TLS certificate file.""")
