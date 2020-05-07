4f8723e9eb6f3bb888e7d4d31dbb6ff886bf3c03
Steven Silvester <stslve@amazon.com>
2020-05-07 15:49:32 -0500

    bump version

--- notebook/_version.py
+++ notebook/_version.py
@@ -3,11 +3,11 @@ store the current version info of the notebook.
 
 """
 
-# Downstream maintainer, when running `python.setup.py jsversion`,
+# Downstream maintainer, when running `python setup.py jsversion`,
 # the version string is propagated to the JavaScript files,  do not forget to
 # patch the JavaScript files in `.postN` release done by distributions.
 
 # Next beta/alpha/rc release: The version number for beta is X.Y.ZbN **without dots**.
 
-version_info = (5, 7, 8)
+version_info = (5, 7, 9)
 __version__ = '.'.join(map(str, version_info[:3])) + ''.join(version_info[3:])
