e946154112daa5ef997d7f549953cce91b336d3d
Min RK <benjaminrk@gmail.com>
2020-11-06 13:52:26 +0100

    Release 6.1.5

--- notebook/static/base/js/namespace.js
+++ notebook/static/base/js/namespace.js
@@ -73,7 +73,7 @@ define(function(){
     // tree
     jglobal('SessionList','tree/js/sessionlist');
 
-    Jupyter.version = "6.1.4";
+    Jupyter.version = "6.1.5";
     Jupyter._target = '_blank';
 
     return Jupyter;
