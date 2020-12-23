fb5deeed23479ff9a790a674993b00d39cc6b225
Zsailer <zsailer@apple.com>
2020-12-23 14:56:23 -0800

    Back to dev version

--- notebook/static/base/js/namespace.js
+++ notebook/static/base/js/namespace.js
@@ -73,7 +73,7 @@ define(function(){
     // tree
     jglobal('SessionList','tree/js/sessionlist');
 
-    Jupyter.version = "6.1.6";
+    Jupyter.version = "7.0.0.dev0";
     Jupyter._target = '_blank';
 
     return Jupyter;
