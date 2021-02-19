5004ba6fa64984a4e21993fbb9d7d0a4dbabd94f
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2021-02-19 09:25:59 +0100

    Switch to an explicit list of resolutions

--- app/style.js
+++ app/style.js
@@ -1,17 +1,1 @@
-import '@jupyterlab-classic/application-extension/style/index.js';
-import '@jupyterlab-classic/notebook-extension/style/index.js';
-import '@jupyterlab-classic/tree-extension/style/index.js';
-import '@jupyterlab-classic/ui-components/style/index.js';
-
-import '@jupyterlab/codemirror-extension/style/index.js';
-import '@jupyterlab/completer-extension/style/index.js';
-import '@jupyterlab/docmanager-extension/style/index.js';
-import '@jupyterlab/fileeditor-extension/style/index.js';
-import '@jupyterlab/mainmenu-extension/style/index.js';
-import '@jupyterlab/notebook-extension/style/index.js';
-import '@jupyterlab/rendermime-extension/style/index.js';
-import '@jupyterlab/running-extension/style/index.js';
-import '@jupyterlab/theme-light-extension/style/index.js';
-import '@jupyterlab/theme-dark-extension/style/index.js';
-import '@jupyterlab/terminal-extension/style/index.js';
-import '@jupyterlab/tooltip-extension/style/index.js';
+import '@jupyterlab/celltags/style/index.js';
