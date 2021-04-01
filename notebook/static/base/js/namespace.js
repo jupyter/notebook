ff851bebac40d530b1a6e5c316e1860d6c20b5a8
parthasarathi0103 <44563306+parthasarathi0103@users.noreply.github.com>
2021-04-01 21:12:18 +0530

    Rc 1.0.1 (#58)
    * Fixed dropdown issue (#57)
    
    * Update _version.py
    
    * Update version
    
    * Vesrion update
    
    * Vesrion upgrade
    
    Co-authored-by: param-d <57786447+param-d@users.noreply.github.com>

--- notebook/static/base/js/namespace.js
+++ notebook/static/base/js/namespace.js
@@ -73,7 +73,7 @@ define(function(){
     // tree
     jglobal('SessionList','tree/js/sessionlist');
 
-    Jupyter.version = "plt.4452.0";
+    Jupyter.version = "1.0.1";
     Jupyter._target = '_blank';
 
     return Jupyter;
