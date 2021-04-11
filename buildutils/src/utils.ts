98d2e067f5a34e7b42d85d93dacaf9438ca2b8be
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2021-04-12 00:11:24 +0200

    Run integrity on release postbump

--- buildutils/src/utils.ts
+++ buildutils/src/utils.ts
@@ -1,6 +1,9 @@
 import { getPythonVersion, run } from '@jupyterlab/buildutils';
 
 export function postbump(): void {
+  // run the integrity
+  run('jlpm integrity');
+
   const newPyVersion = getPythonVersion();
   // Commit changes.
   run(`git commit -am "Release ${newPyVersion}"`);
