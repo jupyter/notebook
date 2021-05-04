2fd108815cf168865839a1196e854ea4e973c27f
Afshin T. Darian <git@darian.af>
2021-05-04 12:03:46 +0100

    Remove jupyter_packaging as a runtime requirement

--- notebook/_version.py
+++ notebook/_version.py
@@ -2,15 +2,18 @@
 store the current version info of the notebook.
 
 """
+import re
+
 # Version string must appear intact for tbump versioning
 __version__ = '6.5.0.dev0'
 
-try:
-    # When installing from a tarball, this import will not yet be available
-    from jupyter_packaging import get_version_info
-    version_info = get_version_info(__version__)
-except ImportError:
-    version_info = ()
+# Build up version_info tuple for backwards compatibility
+pattern = r'(?P<major>\d+).(?P<minor>\d+).(?P<patch>\d+)(?P<rest>.*)'
+match = re.match(pattern, __version__)
+parts = [int(match[part]) for part in ['major', 'minor', 'patch']]
+if match['rest']:
+  parts.append(match['rest'])
+version_info = tuple(parts)
 
 # Downstream maintainer, when running `python.setup.py jsversion`,
 # the version string is propagated to the JavaScript files,  do not forget to
