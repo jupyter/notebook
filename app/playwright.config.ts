6c4648e8c0f3a6ddcbd7437ab216e54f94556c9e
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2021-07-05 19:44:49 +0200

    Test on multiple browsers

--- app/playwright.config.ts
+++ app/playwright.config.ts
@@ -1,12 +1,23 @@
-// playwright.config.ts
+import process from 'process';
+
 import { PlaywrightTestConfig } from '@playwright/test';
 
+import { BrowserName } from './test/utils';
+
+const browserName = (process.env.BROWSER ?? 'chromium') as BrowserName;
+
 const config: PlaywrightTestConfig = {
-  use: {
-    viewport: { width: 1600, height: 900 },
-    slowMo: 100,
-    video: 'on'
-  },
-  outputDir: 'test-results'
+  projects: [
+    {
+      name: 'Retro',
+      use: {
+        viewport: { width: 1600, height: 900 },
+        slowMo: 100,
+        video: 'on',
+        browserName
+      },
+      outputDir: 'test-results'
+    }
+  ]
 };
 export default config;
