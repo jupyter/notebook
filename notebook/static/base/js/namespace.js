f9195b25d736608dff2f2697ac19841914abb604
Steven Silvester <ssilvester@apple.com>
2021-08-06 06:12:29 -0500

    Release 5.7.12

--- notebook/static/base/js/namespace.js
+++ notebook/static/base/js/namespace.js
@@ -73,7 +73,7 @@ define(function(){
     // tree
     jglobal('SessionList','tree/js/sessionlist');
 
-    Jupyter.version = "5.7.11";
+    Jupyter.version = "5.7.12";
     Jupyter._target = '_blank';
     return Jupyter;
 });
