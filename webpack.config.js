f300e92670e22d8d22ca3999fac1b9985c78b23f
Zachary Sailer <zachsailer@gmail.com>
2021-08-10 15:32:53 -0700

    Backport PR #6131: Switch webpack to production mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,27 @@
 const path = require('path');
 
 module.exports = {
-  entry: '@jupyterlab/apputils/lib/sanitizer',
+  entry: ['babel-polyfill', '@jupyterlab/apputils/lib/sanitizer'],
   output: {
     filename: 'index.js',
     path: path.resolve(__dirname, 'notebook/static/components/sanitizer'),
-    libraryTarget: "amd"
+    libraryTarget: "amd",
+  },
+  devtool: false,
+  optimization: {
+    minimize: false
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?jsx?$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          }
+        }
+      }
+    ]
   }
 }
