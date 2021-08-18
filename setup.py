caeb8309cf63de3f2d2f7512d1fdcb96e7dadcaa
Anurag Kumar <mailanu98@gmail.com>
2021-08-18 22:49:26 +0530

    update the version of jinja2
    It will download the latest version of jinja2 available till date jinja2>=2.11.2

--- setup.py
+++ setup.py
@@ -109,7 +109,7 @@ for more information.
     ],
     zip_safe = False,
     install_requires = [
-        'jinja2',
+        'jinja2>=2.11.2',
         'tornado>=6.1',
         # pyzmq>=17 is not technically necessary,
         # but hopefully avoids incompatibilities with Tornado 5. April 2018
