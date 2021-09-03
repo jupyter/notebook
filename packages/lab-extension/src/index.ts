3f49eea5519282f289df00b590a6514b56bd6ff0
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2021-09-03 12:14:42 +0200

    Add buttons to switch to classic lab and retro

--- packages/lab-extension/src/index.ts
+++ packages/lab-extension/src/index.ts
@@ -2,24 +2,171 @@
 // Distributed under the terms of the Modified BSD License.
 
 import {
+  ILabShell,
   JupyterFrontEnd,
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
-import { ICommandPalette } from '@jupyterlab/apputils';
+
+import { CommandToolbarButton, ICommandPalette } from '@jupyterlab/apputils';
+
 import { PageConfig } from '@jupyterlab/coreutils';
+
 import { IMainMenu } from '@jupyterlab/mainmenu';
+
+import { INotebookTracker, NotebookPanel } from '@jupyterlab/notebook';
+
 import { ITranslator } from '@jupyterlab/translation';
-import { interfaceSwitcher } from './interfaceswitcher';
+
+import {
+  jupyterIcon,
+  jupyterFaviconIcon,
+  LabIcon
+} from '@jupyterlab/ui-components';
+
+import { IRetroShell } from '@retrolab/application';
+
+import { retroSunIcon } from '@retrolab/ui-components';
 
 /**
  * The command IDs used by the application plugin.
  */
 namespace CommandIDs {
   /**
-   * Toggle Top Bar visibility
+   * Launch RetroLab Tree
    */
-  export const launchRetroTree = 'retrolab:launchtree';
+  export const launchRetroTree = 'retrolab:launch-tree';
+
+  /**
+   * Open RetroLab
+   */
+  export const openRetro = 'retrolab:open-retro';
+
+  /**
+   * Open in Classic Notebook
+   */
+  export const openClassic = 'retrolab:open-classic';
+
+  /**
+   * Open in JupyterLab
+   */
+  export const openLab = 'retrolab:open-lab';
 }
+
+interface ISwitcherChoice {
+  command: string;
+  commandLabel: string;
+  buttonLabel: string;
+  icon: LabIcon;
+  urlPrefix: string;
+}
+
+/**
+ * A plugin to add custom toolbar items to the notebook page
+ */
+const launchButtons: JupyterFrontEndPlugin<void> = {
+  id: '@retrolab/lab-extension:interface-switcher',
+  autoStart: true,
+  optional: [
+    INotebookTracker,
+    ICommandPalette,
+    IMainMenu,
+    IRetroShell,
+    ILabShell
+  ],
+  activate: (
+    app: JupyterFrontEnd,
+    notebookTracker: INotebookTracker | null,
+    palette: ICommandPalette | null,
+    menu: IMainMenu | null,
+    retroShell: IRetroShell | null,
+    labShell: ILabShell | null
+  ) => {
+    if (!notebookTracker) {
+      // to prevent showing the toolbar button in RetroLab
+      return;
+    }
+
+    const { commands, shell } = app;
+    const baseUrl = PageConfig.getBaseUrl();
+
+    const isEnabled = () => {
+      return (
+        notebookTracker.currentWidget !== null &&
+        notebookTracker.currentWidget === shell.currentWidget
+      );
+    };
+
+    const addInterface = (option: ISwitcherChoice) => {
+      const { command, icon, buttonLabel, commandLabel, urlPrefix } = option;
+      commands.addCommand(command, {
+        label: args => (args.noLabel ? '' : commandLabel),
+        caption: commandLabel,
+        icon,
+        execute: () => {
+          const current = notebookTracker.currentWidget;
+          if (!current) {
+            return;
+          }
+          window.open(`${urlPrefix}${current.context.path}`);
+        },
+        isEnabled
+      });
+
+      if (palette) {
+        palette.addItem({ command, category: 'Other' });
+      }
+
+      if (menu) {
+        menu.viewMenu.addGroup([{ command }], 1);
+      }
+
+      notebookTracker.widgetAdded.connect(
+        async (sender: INotebookTracker, panel: NotebookPanel) => {
+          panel.toolbar.insertBefore(
+            'kernelName',
+            buttonLabel,
+            new CommandToolbarButton({
+              commands,
+              id: command,
+              args: { noLabel: 1 }
+            })
+          );
+          await panel.context.ready;
+          commands.notifyCommandChanged();
+        }
+      );
+    };
+
+    // always add Classic
+    addInterface({
+      command: 'retrolab:open-classic',
+      commandLabel: 'Open in Classic Notebook',
+      buttonLabel: 'openClassic',
+      icon: jupyterIcon,
+      urlPrefix: `${baseUrl}tree/`
+    });
+
+    if (!retroShell) {
+      addInterface({
+        command: 'retrolab:open-retro',
+        commandLabel: 'Open in RetroLab',
+        buttonLabel: 'openRetro',
+        icon: retroSunIcon,
+        urlPrefix: `${baseUrl}retro/tree/`
+      });
+    }
+
+    if (!labShell) {
+      addInterface({
+        command: 'retrolab:open-lab',
+        commandLabel: 'Open in JupyterLab',
+        buttonLabel: 'openLab',
+        icon: jupyterFaviconIcon,
+        urlPrefix: `${baseUrl}doc/tree/`
+      });
+    }
+  }
+};
 
 /**
  * A plugin to add a command to open the RetroLab Tree.
@@ -60,9 +207,6 @@ const launchRetroTree: JupyterFrontEndPlugin<void> = {
 /**
  * Export the plugins as default.
  */
-const plugins: JupyterFrontEndPlugin<any>[] = [
-  launchRetroTree,
-  interfaceSwitcher
-];
+const plugins: JupyterFrontEndPlugin<any>[] = [launchRetroTree, launchButtons];
 
 export default plugins;
