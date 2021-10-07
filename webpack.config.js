dd8ce3acb90244fce7456d6968300a62dda6ccfb
Steven Silvester <ssilvester@apple.com>
2021-10-07 14:31:46 -0500

    workaround for webpack crypto

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,11 @@
 const path = require('path');
+const crypto = require('crypto');
+
+// Workaround for loaders using "md4" by default, which is not supported in FIPS-compliant OpenSSL
+// See https://github.com/jupyterlab/jupyterlab/issues/11248
+const cryptoOrigCreateHash = crypto.createHash;
+crypto.createHash = (algorithm) =>
+  cryptoOrigCreateHash(algorithm == 'md4' ? 'sha256' : algorithm);
 
 module.exports = {
   entry: '@jupyterlab/apputils/lib/sanitizer',
