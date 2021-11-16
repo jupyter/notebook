054b2857c92136db240a79fc0f4dbba9166a4f3a
GitHub Action <41898282+github-actions[bot]@users.noreply.github.com>
2021-11-16 22:58:35 +0000

    Publish 6.4.6
    SHA256 hashes:
    
    notebook-6.4.6-py3-none-any.whl: 5cad068fa82cd4fb98d341c052100ed50cd69fbfb4118cb9b8ab5a346ef27551
    
    notebook-6.4.6.tar.gz: 7bcdf79bd1cda534735bd9830d2cbedab4ee34d8fe1df6e7b946b3aab0902ba3

--- notebook/static/base/js/namespace.js
+++ notebook/static/base/js/namespace.js
@@ -73,7 +73,7 @@ define(function(){
     // tree
     jglobal('SessionList','tree/js/sessionlist');
 
-    Jupyter.version = "6.4.5";
+    Jupyter.version = "6.4.6";
     Jupyter._target = '_blank';
 
     return Jupyter;
