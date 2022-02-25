46afe5014e493665dd2f7b7daa07290cee2b4750
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2022-02-25 09:16:22 +0100

    Remove classic server extension

--- notebook/__init__.py
+++ notebook/__init__.py
@@ -1,5 +1,4 @@
 from ._version import __version__
-from .serverextension import load_jupyter_server_extension
 
 
 def _jupyter_server_extension_paths():
--- notebook/serverextension.py
+++
@@ -1,8 +0,0 @@
-def load_jupyter_server_extension(serverapp):
-    from .app import NotebookApp
-    extension = NotebookApp()
-    extension.serverapp = serverapp
-    extension.load_config_file()
-    extension.update_config(serverapp.config)
-    extension.parse_command_line(serverapp.extra_args)
-    extension.initialize()
