ff15898e4ee80edc7f08c7fb4670db93e9c371fd
Steven Silvester <steven.silvester@ieee.org>
2022-03-09 07:42:21 -0600

    replace missing files

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -119,7 +119,7 @@ language = None
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['examples/Notebook/.ipynb_checkpoints', 'examples/Notebook/nbpackage/*.ipynb']
+exclude_patterns = ['examples/Notebook/.ipynb_checkpoints', 'examples/Notebook/nbpackage/*.ipynb', 'examples/Notebook/nbpackage/nbs/*.ipynb']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
