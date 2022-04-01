f46fa1d0efc99b793f39194bb17fa7beae67deed
Steven Silvester <steven.silvester@ieee.org>
2022-04-01 06:45:08 -0500

    adopt pre-commit

--- docs/jsdoc_plugin.js
+++ docs/jsdoc_plugin.js
@@ -3,7 +3,7 @@ exports.handlers = {
     // e.doclet will refer to the newly created doclet
     // you can read and modify properties of that doclet if you wish
     if (typeof e.doclet.name === 'string') {
-      if (e.doclet.name[0] == '_') {
+      if (e.doclet.name[0] === '_') {
         console.log(
           'Private method "' + e.doclet.longname + '" not documented.'
         );
