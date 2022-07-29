8794be872e5428b82dd57601da3a5e51dbab7c78
GitHub Action <41898282+github-actions[bot]@users.noreply.github.com>
2022-07-29 16:01:22 +0000

    Publish 6.5.0b0
    SHA256 hashes:
    
    notebook-6.5.0b0-py3-none-any.whl: 13a7eed7bb19162b0e36c8a8ed010dd9a0873c40061fd92d7bf9c330437baa53
    
    notebook-6.5.0b0.tar.gz: 264b7925abf90b0396e962bc76a17960cda2c0358b578b817ef159e73272f04d

--- notebook/_version.py
+++ notebook/_version.py
@@ -5,7 +5,7 @@ store the current version info of the notebook.
 import re
 
 # Version string must appear intact for tbump versioning
-__version__ = '6.5.0a0'
+__version__ = '6.5.0b0'
 
 # Build up version_info tuple for backwards compatibility
 pattern = r'(?P<major>\d+).(?P<minor>\d+).(?P<patch>\d+)(?P<rest>.*)'
