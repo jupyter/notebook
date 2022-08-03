b132283d32af0f613a968dbc219d13308e5087f1
RRosio <rosio_reyes@yahoo.com>
2022-08-03 09:37:11 -0700

    Back to dev version

--- notebook/_version.py
+++ notebook/_version.py
@@ -5,7 +5,7 @@ store the current version info of the notebook.
 import re
 
 # Next beta (b)/ alpha (a)/ release candidate (rc) release: The version number for alpha is X.Y.ZaN
-__version__ = '5.7.14a0'
+__version__ = '5.7.14a0.dev'
 
 # Build up version_info tuple for backwards compatibility
 pattern = r'(?P<major>\d+).(?P<minor>\d+).(?P<patch>\d+)(?P<rest>.*)'
