60c38c02a2c5576480c6be6318bfc5e6284f6487
RRosio <rosio_reyes@yahoo.com>
2022-08-03 09:15:14 -0700

    Release 5.7.14a0

--- notebook/static/base/js/namespace.js
+++ notebook/static/base/js/namespace.js
@@ -73,7 +73,7 @@ define(function(){
     // tree
     jglobal('SessionList','tree/js/sessionlist');
 
-    Jupyter.version = "5.7.13";
+    Jupyter.version = "5.7.14a0";
     Jupyter._target = '_blank';
     return Jupyter;
 });
