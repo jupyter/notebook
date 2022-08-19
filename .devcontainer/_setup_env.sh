f7795fe75c108c4399ccad4af7e35fdb334170ec
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2022-08-19 18:48:35 +0200

    wip

--- .devcontainer/_setup_env.sh
+++ .devcontainer/_setup_env.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+curl micro.mamba.pm/install.sh | bash
 source ${HOME}/.bashrc
 micromamba config append channels conda-forge
-micromamba install -n base -y -f ./environment.yml
+micromamba install -n base -y -f /tmp/dev-environment.yml
