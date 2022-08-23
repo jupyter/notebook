dc0c8c47c700123c3f1e43c5f6cb30b2e47a8c86
RRosio <rosio_reyes@yahoo.com>
2022-08-23 09:28:59 -0700

    using group method to match regex items, which works for Python versions < 3.6. Fix so users can install extensions

--- notebook/_version.py
+++ notebook/_version.py
@@ -10,7 +10,7 @@ __version__ = '5.7.14.dev'
 # Build up version_info tuple for backwards compatibility
 pattern = r'(?P<major>\d+).(?P<minor>\d+).(?P<patch>\d+)(?P<rest>.*)'
 match = re.match(pattern, __version__)
-parts = [int(match[part]) for part in ['major', 'minor', 'patch']]
-if match['rest']:
-    parts.append(match['rest'])
+parts = [int(match.group(part)) for part in ['major', 'minor', 'patch']]
+if match.group('rest'):
+    parts.append(match.group('rest'))
 version_info = tuple(parts)
