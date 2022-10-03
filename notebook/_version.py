e8e0f8f32172ab834ce9005277c32ad04fadf8cd
GitHub Action <41898282+github-actions[bot]@users.noreply.github.com>
2022-10-03 15:31:51 +0000

    Publish 7.0.0a6
    SHA256 hashes:
    
    jupyter-notebook-application-7.0.0-alpha.6.tgz: 39d4c07db0c1036e25c27d0b550ab4a051ed34afea12fdb10dee934b3d7600a8
    
    jupyter-notebook-application-extension-7.0.0-alpha.6.tgz: d540f85ab47d363e2ad56bb6787e974abe125987bc0b50893afec6e8f16ac5df
    
    jupyter-notebook-console-extension-7.0.0-alpha.6.tgz: ff43cc82e57400d0041d08f7b920bb8d74a4ad9bcc122dbf4cafd828fa9680d6
    
    jupyter-notebook-docmanager-extension-7.0.0-alpha.6.tgz: 9d36f4ac5a3a5a6c7ce36be123536a3f7cd80624b0c47e0ae2b0232a19d9c22c
    
    jupyter-notebook-documentsearch-extension-7.0.0-alpha.6.tgz: d827f88797953a69818796b63cf492ff6ce12bdbbc07f76fff0bd1ac132be650
    
    jupyter-notebook-help-extension-7.0.0-alpha.6.tgz: b6c9355881c370218b55cf4ca0d248000edd0d6c5a75aedf0f2c30d1e9b14b4c
    
    jupyter-notebook-lab-extension-7.0.0-alpha.6.tgz: ddc1ea09c55f95b6e5808bda1642821af114773662453e4e284403b3da11b928
    
    jupyter-notebook-notebook-extension-7.0.0-alpha.6.tgz: 74e4c5b041b44e42156ddc28ac1a46fd3032948072c18aef646e7f4884615cc5
    
    jupyter-notebook-terminal-extension-7.0.0-alpha.6.tgz: 1d84a5b8738ffed21abaf3c7f18d23042cd14cf7378281c2b6dcd5c9c8fd1f21
    
    jupyter-notebook-tree-7.0.0-alpha.6.tgz: f7f9ee3d3d023c7628a02eb79f2c5e0751a62f15f913b3b4e14334d495f303cc
    
    jupyter-notebook-tree-extension-7.0.0-alpha.6.tgz: b20f220abde4dd8cee3dbdcdd61170b9cee7de51ae21216acfab726ca7bc0e14
    
    jupyter-notebook-ui-components-7.0.0-alpha.6.tgz: 160910258096db6e946f56ebcb3e38ecbebd7e1cccda39304832882c7068afa0
    
    notebook-7.0.0a6-py3-none-any.whl: d14aa27898539cfafa89dc9d13514f7a6a60a22aebd84ca2c272e2f5e8c99ec4
    
    notebook-7.0.0a6.tar.gz: 58122375abddaff5a8bb7e83402857f26d2801a836a0703714d5223be14a094e

--- notebook/_version.py
+++ notebook/_version.py
@@ -4,7 +4,7 @@ import re
 from collections import namedtuple
 
 # Use "hatch version xx.yy.zz" to handle version changes
-__version__ = "7.0.0a5"
+__version__ = "7.0.0a6"
 
 # PEP440 version parser
 _version_regex = re.compile(
