b893cf0d06e8a3740bb7eed5940f25ecb9166ded
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2022-10-06 09:53:56 +0200

    Wait for kernel ready before menu screenshots

--- ui-tests/test/menus.spec.ts
+++ ui-tests/test/menus.spec.ts
@@ -6,6 +6,7 @@ import path from 'path';
 import { test } from './fixtures';
 
 import { expect } from '@playwright/test';
+import { waitForKernelReady } from './utils';
 
 const NOTEBOOK = 'empty.ipynb';
 
@@ -35,6 +36,8 @@ test.describe('Notebook Menus', () => {
   MENU_PATHS.forEach(menuPath => {
     test(`Open menu item ${menuPath}`, async ({ page, tmpPath }) => {
       await page.goto(`notebooks/${tmpPath}/${NOTEBOOK}`);
+      await waitForKernelReady(page);
+
       await page.menu.open(menuPath);
       expect(await page.menu.isOpen(menuPath)).toBeTruthy();
 
