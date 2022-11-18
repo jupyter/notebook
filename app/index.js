47b0c4fe8ed77d640b4b024337d8cb362f97fa74
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2022-11-18 14:38:05 +0000

    Fix plugin name for `@jupyterlab/apputils-extension:sanitizer`

--- app/index.js
+++ app/index.js
@@ -84,7 +84,7 @@ async function main() {
     require('@jupyterlab/apputils-extension').default.filter(({ id }) =>
       [
         '@jupyterlab/apputils-extension:palette',
-        '@jupyter/apputils-extension:sanitizer',
+        '@jupyterlab/apputils-extension:sanitizer',
         '@jupyterlab/apputils-extension:settings',
         '@jupyterlab/apputils-extension:state',
         '@jupyterlab/apputils-extension:themes',
