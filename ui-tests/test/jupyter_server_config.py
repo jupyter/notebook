48fb973071203e8fc4ef8b41c0c2cfc0bc6d4ed3
Steven Silvester <steven.silvester@ieee.org>
2022-12-19 07:37:55 -0600

    CI Cleanup (#6667)
    * ci cleanup
    
    * lint
    
    * run ui tests on ubuntu 20.04

--- ui-tests/test/jupyter_server_config.py
+++ ui-tests/test/jupyter_server_config.py
@@ -2,13 +2,13 @@ from tempfile import mkdtemp
 from typing import Any
 
 c: Any
-c.ServerApp.port = 8888
-c.ServerApp.port_retries = 0
-c.ServerApp.open_browser = False
+c.ServerApp.port = 8888  # noqa
+c.ServerApp.port_retries = 0  # noqa
+c.ServerApp.open_browser = False  # noqa
 
-c.ServerApp.root_dir = mkdtemp(prefix="galata-test-")
+c.ServerApp.root_dir = mkdtemp(prefix="galata-test-")  # noqa
 c.ServerApp.token = ""  # noqa
 c.ServerApp.password = ""  # noqa
-c.ServerApp.disable_check_xsrf = True
+c.ServerApp.disable_check_xsrf = True  # noqa
 
-c.JupyterNotebookApp.expose_app_in_browser = True
+c.JupyterNotebookApp.expose_app_in_browser = True  # noqa
