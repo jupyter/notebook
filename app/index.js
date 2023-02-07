27c2bfbb80d42a3fd2541b13fae6143e2e91e4dd
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2023-02-07 14:52:18 +0100

    Update to JupyterLab 4.0.0a33 (#6713)
    * Update to JupyterLab 4.0.0a33
    
    * Update Lumino packages
    
    * Update to React 18
    
    * Fix default browser
    
    * Add defaultFileBrowser plugin
    
    * Update yarn.lock
    
    * Require Python 3.8
    
    * Remove docprovider
    
    * Bump Python dependencies
    
    * Move Python bumps
    
    * Unbump `jupyterlab_server`
    
    * Align `jupyterlab_server`dep
    
    * Bump jupyterlab_server
    
    * Try with 3.10
    
    * Manually set checkpoint text in UI tests
    
    * Fix checkpoints value in UI tests
    
    * Update snapshots

--- app/index.js
+++ app/index.js
@@ -117,12 +117,14 @@ async function main() {
         '@jupyterlab/docmanager-extension:manager'
       ].includes(id)
     ),
-    require('@jupyterlab/docprovider-extension'),
     require('@jupyterlab/documentsearch-extension').default.filter(({ id }) =>
       ['@jupyterlab/documentsearch-extension:plugin'].includes(id)
     ),
     require('@jupyterlab/filebrowser-extension').default.filter(({ id }) =>
-      ['@jupyterlab/filebrowser-extension:factory'].includes(id)
+      [
+        '@jupyterlab/filebrowser-extension:factory',
+        '@jupyterlab/filebrowser-extension:defaultFileBrowser'
+      ].includes(id)
     ),
     require('@jupyterlab/fileeditor-extension').default.filter(({ id }) =>
       ['@jupyterlab/fileeditor-extension:plugin'].includes(id)
