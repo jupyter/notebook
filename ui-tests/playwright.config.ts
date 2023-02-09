61b2ad27ea490c72e46b43cd83f3393a488adb78
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2023-02-09 09:50:23 +0100

    Use Playwright `webServer` (#6720)
    * Use Playwright `webServer`
    
    * Try `jlpm start`
    
    * Lint

--- ui-tests/playwright.config.ts
+++ ui-tests/playwright.config.ts
@@ -6,5 +6,13 @@ module.exports = {
     appPath: '',
     video: 'retain-on-failure'
   },
-  retries: 1
+  retries: 1,
+  webServer: [
+    {
+      command: 'jlpm start',
+      port: 8888,
+      timeout: 120 * 1000,
+      reuseExistingServer: true
+    }
+  ]
 };
