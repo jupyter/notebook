d0ecb74a1bf92b9de916455b1aeac7db787ca4b2
Eric Charles <eric@datalayer.io>
2023-02-20 18:12:03 +0100

    Add a banner and log to information about the migration to Notebook 7 plan (#6742)
    * Add a banner to inform about the migration plan to notebook 7
    
    * remove trailing spaces

--- notebook/notebookapp.py
+++ notebook/notebookapp.py
@@ -185,6 +185,20 @@ class NotebookWebApplication(web.Application):
         if settings['autoreload']:
             log.info('Autoreload enabled: the webapp will restart when any Python src file changes.')
 
+        print("""
+  _   _          _      _
+ | | | |_ __  __| |__ _| |_ ___
+ | |_| | '_ \/ _` / _` |  _/ -_)
+  \___/| .__/\__,_\__,_|\__\___|
+       |_|
+                       
+Read the migration plan to Notebook 7 to learn about the new features and the actions to take if you are using extensions.
+
+https://jupyter-notebook.readthedocs.io/en/latest/migrate_to_notebook7.html
+
+Please note that updating to Notebook 7 might break some of your extensions.
+""")
+
         super().__init__(handlers, **settings)
 
     def init_settings(self, jupyter_app, kernel_manager, contents_manager,
