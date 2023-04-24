fa1f3e42b224fc9a5c6ae30335f15cbbe4648e3b
Min RK <benjaminrk@gmail.com>
2023-04-24 09:38:42 +0200

    fix import of warnings.warn (#6840)
    * fix import of warnings.warn
    
    never part of the traitlets API
    
    * remove yanked codecov

--- notebook/traittypes.py
+++ notebook/traittypes.py
@@ -1,5 +1,6 @@
 import inspect
-from traitlets import ClassBasedTraitType, Undefined, warn
+from warnings import warn
+from traitlets import ClassBasedTraitType, Undefined
 
 # Traitlet's 5.x includes a set of utilities for building
 # description strings for objects. Traitlets 5.x does not
