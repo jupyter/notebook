fe35f939b5a2c256cbc6822a60ea728d6f5d4251
Rosio <rosio_reyes@yahoo.com>
2023-07-27 10:49:43 -0700

    pin jupyter_client<8 and pyzmq<25 (#6749)

--- setup.py
+++ setup.py
@@ -109,11 +109,11 @@ for more information.
         'tornado>=6.1',
         # pyzmq>=17 is not technically necessary,
         # but hopefully avoids incompatibilities with Tornado 5. April 2018
-        'pyzmq>=17',
+        'pyzmq>=17,<25',
         'argon2-cffi',
         'traitlets>=4.2.1',
         'jupyter_core>=4.6.1',
-        'jupyter_client>=5.3.4',
+        'jupyter_client>=5.3.4,<8',
         'ipython_genutils',
         'nbformat',
         'nbconvert>=5',
