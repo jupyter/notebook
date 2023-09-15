068175ab0ccdf920d3ca211aeeaa1d13316b96f5
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2023-09-15 09:36:40 +0200

    Update to JupyterLab 4.0.6 (#7049)
    * Update to JupyterLab 4.0.6
    
    * fix app_version
    
    * reuse upstream config

--- ui-tests/test/jupyter_server_config.py
+++ ui-tests/test/jupyter_server_config.py
@@ -1,18 +1,8 @@
-from pathlib import Path
-from tempfile import mkdtemp
 from typing import Any
 
-import jupyterlab
+from jupyterlab.galata import configure_jupyter_server
 
 c: Any
-c.ServerApp.port = 8888
-c.ServerApp.port_retries = 0
-c.ServerApp.open_browser = False
-
-c.ServerApp.root_dir = mkdtemp(prefix="galata-test-")
-c.ServerApp.token = ""
-c.ServerApp.password = ""
-c.ServerApp.disable_check_xsrf = True
-
 c.JupyterNotebookApp.expose_app_in_browser = True
-c.LabServerApp.extra_labextensions_path = str(Path(jupyterlab.__file__).parent / "galata")
+
+configure_jupyter_server(c)
