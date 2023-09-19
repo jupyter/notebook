05801fd23c48bd61aeea2b2c9b7d7239e1ceff02
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2023-09-19 07:41:42 +0000

    fix typo

--- packages/application/src/pathopener.ts
+++ packages/application/src/pathopener.ts
@@ -13,8 +13,8 @@ class DefaultNotebookPathOpener implements INotebookPathOpener {
    * Open a path in a new browser tab.
    */
   open(options: INotebookPathOpener.IOpenOptions): WindowProxy | null {
-    const { prefix: route, path, searchParams, target, features } = options;
-    const url = new URL(URLExt.join(route, path ?? ''), window.location.origin);
+    const { prefix, path, searchParams, target, features } = options;
+    const url = new URL(URLExt.join(prefix, path ?? ''), window.location.origin);
     if (searchParams) {
       url.search = searchParams.toString();
     }
