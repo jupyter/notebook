2bba503cd053b88f1e3bf2c8d6b7fced4cb7d637
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2024-04-09 09:07:52 +0200

    Default to defer for now

--- packages/notebook-extension/src/index.ts
+++ packages/notebook-extension/src/index.ts
@@ -564,6 +564,30 @@ const editNotebookMetadata: JupyterFrontEndPlugin<void> = {
 };
 
 /**
+ * A plugin to set the default windowing mode for the notebook
+ */
+const windowing: JupyterFrontEndPlugin<void> = {
+  id: '@jupyter-notebook/notebook-extension:windowing',
+  autoStart: true,
+  requires: [ISettingRegistry],
+  activate: (app: JupyterFrontEnd, settingRegistry: ISettingRegistry): void => {
+    // default to `none` to avoid notebook rendering glitches
+    const settings = settingRegistry.load(
+      '@jupyterlab/notebook-extension:tracker'
+    );
+    Promise.all([settings, app.restored])
+      .then(([settings]) => {
+        if (settings.user.windowing === undefined) {
+          void settings.set('windowingMode', 'defer');
+        }
+      })
+      .catch((reason: Error) => {
+        console.error(reason.message);
+      });
+  },
+};
+
+/**
  * Export the plugins as default.
  */
 const plugins: JupyterFrontEndPlugin<any>[] = [
@@ -576,6 +600,7 @@ const plugins: JupyterFrontEndPlugin<any>[] = [
   scrollOutput,
   tabIcon,
   trusted,
+  windowing,
 ];
 
 export default plugins;
