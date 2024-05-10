aece614683b834b1a41a1c2de5f684c89ac17d37
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2024-05-10 09:43:45 +0000

    update UI tests

--- ui-tests/test/general.spec.ts
+++ ui-tests/test/general.spec.ts
@@ -43,6 +43,9 @@ test.describe('General', () => {
     // hover somewhere else to make the add cell disappear
     await page.hover('#jp-top-bar');
 
+    // click to make the blue border around the cell disappear
+    await page.click('.jp-WindowedPanel-outer');
+
     // special case for firefox headless issue
     // see https://github.com/jupyter/notebook/pull/6872#issuecomment-1549594166 for more details
     if (browserName === 'firefox') {
