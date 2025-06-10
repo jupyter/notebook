ee807a13e7d5014e4b8c98998220d713d6e1c122
muazislambabar <muazislambabar@gmail.com>
2025-06-10 10:28:47 +0000

    removed some content
    FOr studies purposes

--- notebook/__init__.py
+++ notebook/__init__.py
@@ -2,12 +2,7 @@ from __future__ import annotations
 
 from typing import Any
 
-from ._version import __version__, version_info  # noqa: F401
-
-
-def _jupyter_server_extension_paths() -> list[dict[str, str]]:
-    return [{"module": "notebook"}]
-
+from .
 
 def _jupyter_server_extension_points() -> list[dict[str, Any]]:
     from .app import JupyterNotebookApp
