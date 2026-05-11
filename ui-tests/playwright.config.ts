722977573d9fde515ee054ff93f16ddf665cf850
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2026-05-11 12:04:18 +0200

    UI tests HTML report (#7913)
    * UI tests HTML report
    
    * zizmor
    
    * more fixes

--- ui-tests/playwright.config.ts
+++ ui-tests/playwright.config.ts
@@ -1,7 +1,18 @@
 import baseConfig from '@jupyterlab/galata/lib/playwright-config';
 
+const blobOptions = process.env.BLOB_FILENAME
+  ? { fileName: process.env.BLOB_FILENAME }
+  : {};
+
 module.exports = {
   ...baseConfig,
+  tag: process.env.PLAYWRIGHT_TEST_TAG,
+  reporter: process.env.CI
+    ? [
+        ['blob', blobOptions],
+        ['json', { outputFile: 'test-results/report.json' }],
+      ]
+    : [['list'], ['html', { open: 'on-failure' }]],
   use: {
     appPath: '',
     trace: 'on-first-retry',
