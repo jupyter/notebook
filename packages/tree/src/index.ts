7f394c41629b14c6005b517372131a9fee6bbc85
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2026-07-31 18:23:02 +0200

    Generate style files and check packages with ensurePackage

--- packages/tree/src/index.ts
+++ packages/tree/src/index.ts
@@ -1,2 +1,9 @@
+// Copyright (c) Jupyter Development Team.
+// Distributed under the terms of the Modified BSD License.
+/**
+ * @packageDocumentation
+ * @module tree
+ */
+
 export * from './notebook-tree';
 export * from './token';
