9c8d7c24006554dda8c5fc02c69f91d6449c712a
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2026-09-21 19:55:41 +0200

    Update to JupyterLab v4.6.4 (#8066)
    * Update to JupyterLab v4.6.4
    
    * Update the copyright year to 2026
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- packages/help-extension/src/index.tsx
+++ packages/help-extension/src/index.tsx
@@ -150,7 +150,7 @@ const about: JupyterFrontEndPlugin<void> = {
           </span>
         );
         const version = trans.__('Version: %1', app.version);
-        const copyright = trans.__('© 2021-2025 Jupyter Notebook Contributors');
+        const copyright = trans.__('© 2021-2026 Jupyter Notebook Contributors');
         const body = (
           <>
             <span className="jp-AboutNotebook-version">{version}</span>
